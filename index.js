23de2ece51e5a0eb1544a80b8b8a68cd2e3573a2
Dana Shacham <dshacham22@gmail.com>
2020-01-31 15:38:19 +0100

    add js fix html css

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 (() => {
+    const section = document.querySelector('section');
+    const close = document.querySelector('span');
 
+    const subPop = function (event) {
+        if (event.clientY < 10) {
+            section.classList.add('popup');
+        };
+    };
+    document.body.addEventListener('mousemove', subPop, false);
+
+    const closePop = function () {
+        section.classList.remove('popup');
+    };
+
+    close.addEventListener('click', closePop, false);
+    document.body.addEventListener('click', closePop, false);
 })();
